2f2be0c006dfacd6b52c290d7e85ba73b5c11f69
Rica Feng <topgb@msn.com>
2015-11-30 04:20:22 -0800

    backward selection

--- CS170/assn2/nearestneighbor.cpp
+++ CS170/assn2/nearestneighbor.cpp
@@ -12,6 +12,11 @@
 
 using namespace std;
 
+//subdivision
+#define K 10
+//check this number of nearest k neghbors
+#define NEARESTK 21
+
 int maxFeatures = 64;
 int numbFeatures = 0;
 int maxInstances = 2048;
@@ -21,7 +26,7 @@ int numbInstances = 0;
 
 enum Algorithm {ForwardSelection, BackwardElimination, Ricarithm, All};
 
-//vertex on a graph
+//point on a graph
 struct Point
 {
 	Point(vector<double> c)
@@ -29,6 +34,7 @@ struct Point
 	vector<double> coordinates;
 };
 
+//stores points
 struct Graph
 {
 	vector<pair<int, Point> > points;
@@ -38,10 +44,9 @@ struct Graph
 struct Container
 {
 	vector <vector<double> > features;
-	vector<double> averages;
-	vector<double> standarddeviations;
 };
 
+//only 2 classes
 Container c1;
 Container c2;
 
@@ -121,7 +126,7 @@ void readFile(string filename)
 		//check all features for this row until new line or end of file
 		for (unsigned int i = 0; i < numbFeatures; ++i)
 		{
-			cout << setprecision(13);
+			cout << setprecision(20);
 			f >> value;
 			//cout << "class: " << c << endl;
 			//cout << "value: " << value << endl << endl;
@@ -191,24 +196,36 @@ void normalizeData()
 {
 	for (unsigned int i = 0; i < numbFeatures; ++i)
 	{
+		//holds all data from both classes about 1 feature
+		vector<double> combinedData;
+		
 		//cout << c1.features[i].size() << endl;
 		//cout << c2.features[i].size() << endl;
-		//get averages
-		c1.averages.push_back(average(c1.features[i]));
-		c2.averages.push_back(average(c2.features[i]));
-		//get stddev
-		c1.standarddeviations.push_back(deviation(c1.features[i], c1.averages[i]));
-		c2.standarddeviations.push_back(deviation(c2.features[i], c2.averages[i]));
+		
+		//fill up combinedData with information about 1 feature 
+		for (unsigned int j = 0; j < c1.features[j].size(); ++j)
+		{
+			combinedData.push_back(c1.features[i][j]);
+		}
+		for (unsigned int j = 0; j < c2.features[j].size(); ++j)
+		{
+			combinedData.push_back(c2.features[i][j]);
+		}
+		//get average and deviation for this feature
+		double featureAverage = average(combinedData);
+		double featureDeviation = deviation(combinedData, featureAverage);
+		
+		//normalize each data value for that feature
 		for (unsigned int j = 0; j < c1.features[i].size(); ++j)
 		{
 			//cout << "before value: " << c1.features[i][j] << endl;
-			c1.features[i][j] = znormalize(c1.features[i][j], c1.averages[i], c1.standarddeviations[i]);
+			c1.features[i][j] = znormalize(c1.features[i][j], featureAverage, featureDeviation);
 			//cout << "normalized value: " << c1.features[i][j] << endl;
 		}
 		for (unsigned int j = 0; j < c2.features[i].size(); ++j)
 		{
 			//cout << "before value: " << c2.features[i][j] << endl;
-			c2.features[i][j] = znormalize(c2.features[i][j], c2.averages[i], c2.standarddeviations[i]);
+			c2.features[i][j] = znormalize(c2.features[i][j], featureAverage, featureDeviation);
 			//cout << "normalized value: " << c2.features[i][j] << endl;
 		}
 	}
@@ -254,10 +271,9 @@ double leaveOneOutEvaluation(int k, vector<int> f)
 	}	
 	//shuffle all coordinates in graph
 	random_shuffle(g.points.begin(), g.points.end());
-	cout << "graph size " << g.points.size() << endl;
+	//cout << "graph size " << g.points.size() << endl;
 	
-	
-	//current testcase (subdivision)
+	//starting testcase (subdivision)
 	int minindex = 0;
 	int maxindex = k - 1;
 	
@@ -272,7 +288,7 @@ double leaveOneOutEvaluation(int k, vector<int> f)
 		//loop through all valid points to find closest point to current point
 		for (unsigned int i = minindex; i < maxindex; ++i)
 		{
-			cout << "begin check" << endl;
+			//cout << "begin check" << endl;
 			//automatically sorts points by smallest distance to largest
 			priority_queue<pair<int, double>, vector<pair<int, double> >, Compare > closelist;
 			for (unsigned int j = 0; j < numbInstances; ++j)
@@ -290,15 +306,15 @@ double leaveOneOutEvaluation(int k, vector<int> f)
 			//kth - 1 smallest elements,
 			int correct = 0;
 			int incorrect = 0;
-			for (unsigned int j = 0; j < k - 1; ++j)
+			for (unsigned int j = 0; j < NEARESTK; ++j)
 			{
-				cout << "check distance: " << closelist.top().second << endl;
+				//cout << "check distance: " << closelist.top().second << endl;
 				if (closelist.top().first == g.points[i].first) correct++;
 				else incorrect++;
 				closelist.pop();
 			}
-			cout << "c: " << correct << endl;
-			cout << "ic: " << incorrect << endl;
+			//cout << "c: " << correct << endl;
+			//cout << "ic: " << incorrect << endl;
 			if (correct > incorrect) numbCorrect++;
 		}
 		//move test set
@@ -306,16 +322,177 @@ double leaveOneOutEvaluation(int k, vector<int> f)
 		maxindex += k;
 	}
 	//cout << "correct: " << numbCorrect << endl;
-	return numbCorrect / g.points.size();
+	return numbCorrect / numbInstances;
+}
+
+//displays set of numbers
+void displaySet(vector<int> v)
+{
+	for (unsigned int i = 0; i < v.size(); ++i)
+	{
+		cout << v[i];
+		if (i != v.size() - 1) cout << ", ";
+	}
 }
 
 //forward selection algorithm
 void forwardSelection()
 {
+	//vector for keeping track of best features
+	vector<int> answer;
+	//vector for testing current features
 	vector<int> testfeatures;
-	testfeatures.push_back(0);
-	cout << "percentage of first feature and second feature: " << endl;
-	cout << leaveOneOutEvaluation(10, testfeatures) << endl;
+	vector<int> bestfeatures;
+	
+	//current percentage
+	double currpercentage;
+	//best percentage for this set of features
+	double bestpercentage;
+	//feature number of best
+	double bestfeatureIndex;
+	
+	//best overall percentage
+	double answerpercentage = 0;
+	
+	bool keepfeature = true;
+	
+	cout << "Beginning Search." << endl;
+	for (unsigned x = 0; x < numbFeatures; ++x)
+	{
+		bestpercentage = 0;
+		bestfeatureIndex = 0;
+		currpercentage = 0;
+		
+		//check next best feature to add in
+		for (unsigned int i = 0; i < numbFeatures; ++i)
+		{
+			keepfeature = true;
+			//check to see if we already have this feature
+			for (unsigned int j = 0; j < testfeatures.size(); ++j)
+			{
+				if (testfeatures[j] == i) keepfeature = false;
+			}
+			//only check accuracy if we did not check this feature yet
+			if (keepfeature)
+			{
+				testfeatures.push_back(i);
+				
+				cout << "\tUsing feature(s) {";
+				displaySet(testfeatures);
+				cout << "} accuracy is ";
+				currpercentage = leaveOneOutEvaluation(K, testfeatures);
+				cout << currpercentage * 100.0 << "%" << endl;
+				
+				if (currpercentage > bestpercentage)
+				{
+					bestpercentage = currpercentage;
+					bestfeatureIndex = testfeatures[testfeatures.size() - 1];
+					bestfeatures = testfeatures;
+				}
+				testfeatures.pop_back();
+			}
+		}
+		testfeatures.push_back(bestfeatureIndex);
+		
+		if (bestpercentage > answerpercentage)
+		{
+			answer = bestfeatures;
+			answerpercentage = bestpercentage;
+		}
+		else
+		{
+			cout << "(Warning, Accuracy has decreased! Continuing search in case of local maxima)" << endl;
+		}
+		cout << "Feature set {";
+		displaySet(testfeatures);
+		cout << "} was best, accuracy is " << bestpercentage * 100.0 << "%" << endl;
+	}
+	cout << "Finished search!! The best feature subset is {";
+	displaySet(answer);
+	cout << "}, which has an accuracy of " << answerpercentage * 100.0 << "%" << endl;
+}
+
+void backwardElimination()
+{
+	//vector for keeping track of best features
+	vector<int> answer;
+	//vector for testing current features
+	vector<int> testfeatures;
+	for (unsigned int i = 0; i < numbFeatures; ++i)
+	{
+		testfeatures.push_back(i);
+	}
+	vector<int> bestfeatures;
+	
+	//current percentage
+	double currpercentage;
+	//best percentage for this set of features
+	double bestpercentage;
+	//feature number of best
+	double bestfeatureIndex;
+	
+	//best overall percentage
+	double answerpercentage = leaveOneOutEvaluation(K, testfeatures);
+	
+	cout << "Beginning Search." << endl;
+	
+	cout << "\tUsing ALL feature(s) {";
+	displaySet(testfeatures);
+	cout << "} accuracy is " << answerpercentage * 100.0 << "%" << endl;
+	
+	while (testfeatures.size() != 0)
+	{
+		bestpercentage = 0;
+		bestfeatureIndex = 0;
+		currpercentage = 0;
+		
+		//check next best feature to add in
+		for (unsigned int i = 0; i < testfeatures.size(); ++i)
+		{
+			//cout << "Removing Element: " << testfeatures[i] << endl;
+			//erase the ith element, then test that
+			double save = testfeatures[i];
+			testfeatures.erase(testfeatures.begin() + i);
+			
+			cout << "\tUsing feature(s) {";
+			displaySet(testfeatures);
+			cout << "} accuracy is ";
+			
+			currpercentage = leaveOneOutEvaluation(K, testfeatures);
+			cout << currpercentage * 100.0 << "%" << endl;
+			if (currpercentage > bestpercentage)
+			{
+				bestpercentage = currpercentage;
+				bestfeatureIndex = i;
+				bestfeatures = testfeatures;
+			}
+			
+			//add the element back on, then sort again
+			//cout << "Adding Element" << save << endl;
+			testfeatures.push_back(save);
+			sort(testfeatures.begin(), testfeatures.end());
+			//cout << "sdf" << endl;
+		}
+		
+		cout << "best fe in: " << bestfeatureIndex << endl;
+		//remove the element that will increase accuracy the most
+		testfeatures.erase(testfeatures.begin() + bestfeatureIndex);
+		if (bestpercentage > answerpercentage)
+		{
+			answer = bestfeatures;
+			answerpercentage = bestpercentage;
+		}
+		else
+		{
+			cout << "(Warning, Accuracy has decreased! Continuing search in case of local maxima)" << endl;
+		}
+		cout << "Feature set {";
+		displaySet(bestfeatures);
+		cout << "} was best, accuracy is " << bestpercentage * 100.0 << "%" << endl;
+	}
+	cout << "Finished search!! The best feature subset is {";
+	displaySet(answer);
+	cout << "}, which has an accuracy of " << answerpercentage * 100.0 << "%" << endl;
 }
 
 int main(int argc, char *argv[])
@@ -329,7 +506,7 @@ int main(int argc, char *argv[])
 	//if there is any command arg, automiatically run tests
 	if (argc != 1)
 	{
-		filename = "cs_170_small15.txt";
+		filename = "cs_170_small80.txt";
 		algorithm = 3;
 		numbFeatures = initNumbFeatures(filename);
 		initFeatures();
@@ -360,18 +537,20 @@ int main(int argc, char *argv[])
 	normalizeData();
 	cout << "Done!" << endl << endl;
 	
-	//choose correct algorithm, or run all algorithms
+	//choose specified algorithm, or run all algorithms
 	switch (algorithm)
 	{
 		case ForwardSelection:
 			forwardSelection();
 			break;
 		case BackwardElimination:
+			backwardElimination();
 			break;
 		case Ricarithm:
 			break;
 		case All:
-			forwardSelection();
+			//forwardSelection();
+			backwardElimination();
 			break;
 	}
 }
